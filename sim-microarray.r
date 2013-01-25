13132ef3980f85252ba18b61187868f876bd000d
John Ramey <johnramey@gmail.com>
2013-01-24 21:14:28 -0800

    Removed RDA classifier from simulation.

--- sim-microarray.r
+++ sim-microarray.r
@@ -2,9 +2,9 @@ library('ProjectTemplate')
 load.project()
 
 set.seed(42)
-num_cores <- 1
+num_cores <- 7
 
-data_sets <- c("burczynski", "christensen", "khan", "nakayama", "sorlie", "su", "sun", "yeoh")
+data_sets <- c("burczynski", "christensen", "khan", "nakayama", "sorlie", "su", "yeoh")
 
 
 results <- mclapply(data_sets, function(data_set) {
@@ -27,20 +27,15 @@ results <- mclapply(data_sets, function(data_set) {
     Witten_out <- Witten_Tibshirani(train_x, train_y, test_x)
     Witten_errors <- sum(Witten_out$predictions != test_y)
 
-    # Applies the variable selection from Witten and Tibshirani
+    # Applies the variable selection from Witten and Tibshirani (2011)
     train_x <- train_x[, Witten_out$variables]
     test_x <- test_x[, Witten_out$variables]
 
-    # Hard-ETRDA
-    cv_out <- etrda_cv(x = train_x, y = train_y, threshold = "hard", num_delta = 10, prior = prior_probs)
-    etrda_out <- with(cv_out, etrda(x = train_x, y = train_y, threshold = "hard", lambda = lambda, delta = delta, prior = prior_probs))
-    etrda_hard_errors <- sum(predict(etrda_out, test_x)$class != test_y)
+    # ETRDA
+    cv_out <- etrda_cv(x = train_x, y = train_y, prior = prior_probs)
+    etrda_out <- with(cv_out, etrda(x = train_x, y = train_y, lambda = lambda, delta = delta, prior = prior_probs))
+    etrda_errors <- sum(predict(etrda_out, test_x)$class != test_y)
 
-    # RDA from Friedman (1989)
-    cv_out <- rda_cv(x = train_x, y = train_y, prior = prior_probs)
-    rda_out <- with(cv_out, rda(x = train_x, y = train_y, lambda = lambda, gamma = gamma, prior = prior_probs))
-    rda_errors <- sum(predict(rda_out, test_x)$class != test_y)
-    
     # DLDA and DQDA
     dlda_errors <- sum(predict(dlda(x = train_x, y = train_y, prior = prior_probs), test_x)$class != test_y)
     dqda_errors <- sum(predict(dqda(x = train_x, y = train_y, prior = prior_probs), test_x)$class != test_y)
@@ -48,8 +43,7 @@ results <- mclapply(data_sets, function(data_set) {
     list(
       dlda = dlda_errors,
       dqda = dqda_errors,
-      etrda_hard = etrda_hard_errors,
-      rda = rda_errors,
+      etrda = etrda_errors,
       Witten = Witten_errors
       )
   })
@@ -62,6 +56,4 @@ results <- mclapply(data_sets, function(data_set) {
 results <- do.call(rbind, results)
 
 save(results, file = 'results-microarray.RData')
-
-
 
