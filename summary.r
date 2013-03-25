a1d450781f1759c1b1f2cce6e64bde5f590db6c2
John Ramey <johnramey@gmail.com>
2013-03-25 10:12:04 -0700

    Added simulation results, a discussion of the results, and a discussion to the paper.

--- summary.r
+++ summary.r
@@ -2,28 +2,21 @@ library('ProjectTemplate')
 load.project()
 
 # Remove the following data sets because there is too much separation for a good comparison
-data_sets <- c("burczynski", "nakayama", "shipp", "singh")
-results <- subset(results, data_set %in% data_sets)
+results <- na.omit(results)
 
-results$q <- as.factor(as.numeric(results$q))
-results$Error <- as.numeric(results$Error)
+results$d <- as.factor(results$d)
 results$data_set <- as.character(results$data_set)
 
-
-# Removes the following classifiers from consideration
-results <- subset(results, !Classifier %in% c("Cao", "sdqda"))
-
-p <- ggplot(results, aes(x = q, y = Error))
+p <- ggplot(results, aes(x = d, y = Error))
 p <- p + geom_boxplot(aes(fill = Classifier))
 p + facet_wrap(~ data_set)
 
-
-results_summary <- ddply(results, .(data_set, q, Classifier), summarize,
+results_summary <- ddply(results, .(data_set, d, Classifier), summarize,
                          Avg_Error = mean(Error),
-                         SE_Error = sqrt(Avg_Error * (1 - Avg_Error) / length(Error)))
+                         SE_Error = sqrt(mean(Error) * (1 - mean(Error)) / length(Error)))
 
 # Table containing a summary of the data sets
-data_summary <- subset(describe_data(), author %in% data_sets)
+data_summary <- subset(describe_data(), author %in% unique(results$data_set))
 data_summary$K[data_summary$author == "nakayama"] <- 5
 rownames(data_summary) <- NULL
 colnames(data_summary)[1:3] <- c("Author", "Year", "N")
@@ -36,19 +29,19 @@ print(xtable(data_summary, caption = "Summary of high-dimensional microarray dat
 
 # Tables containing the classification results for each value of 'q'
 results_summary$data_set <- Hmisc:::capitalize(results_summary$data_set)
-results_summary$q <- as.factor(results_summary$q)
+results_summary$d <- as.factor(results_summary$d)
 results_summary$Avg_Error <- round_digits(results_summary$Avg_Error, 3)
 results_summary$SE_Error <- round_digits(results_summary$SE_Error, 3)
 results_summary$Error <- with(results_summary, paste0(Avg_Error, " (", SE_Error, ")"))
 results_summary <- subset(results_summary, select = -c(Avg_Error, SE_Error))
+colnames(results_summary) <- c("Dataset", "d", "Classifier", "Error")
 
-results_tables <- tapply(seq_along(results_summary$q), results_summary$q, function(i) {
-  dcast(results_summary[i, ], data_set ~ Classifier, value.var = "Error")
+results_tables <- tapply(seq_along(results_summary$d), results_summary$d, function(i) {
+  dcast(results_summary[i, ], Dataset ~ Classifier, value.var = "Error")
 })
 
-
-results_xtables <- lapply(names(results_tables), function(q) {
+results_xtables <- lapply(names(results_tables), function(d) {
   tab_caption <- "The average of the test error rates for the microarray data sets with $d = "
-  tab_caption <- paste0(tab_caption, q, "$. Approximate standard errors are given in parentheses.")
-  xtable(results_tables[[q]], caption = tab_caption, label = paste0("tab:results-d", q))
+  tab_caption <- paste0(tab_caption, d, "$. Approximate standard errors are given in parentheses.")
+  xtable(results_tables[[d]], caption = tab_caption, label = paste0("tab:results-d", d))
 })
