b071f57784d6d027e48ee81f0aeee0b2e62491cd
John Ramey <johnramey@gmail.com>
2015-03-30 06:28:23 -0500

    Updated README. Added requirements script

---
+++ README.md
@@ -0,0 +1,15 @@
+# High-Dimensional Regularized Discriminant Analysis
+
+This repository includes the paper and simulation code.
+
+## Installation
+
+In order to reproduce the simulations and build the article, requisite R
+packages must be installed. The `requirements.r` file is a script that will
+install necessary packages. Either run the source or from the CLI:
+
+```bash
+Rscript requirements.r
+```
+
+
---
+++ requirements.r
@@ -0,0 +1,12 @@
+source("http://bioconductor.org/biocLite.R")
+biocLite("multtest", ask=FALSE)
+
+cran_packages <- c('futile.logger', 'reshape2', 'plyr', 'ggplot2', 'stringr',
+                   'rda', 'penalizedLDA', 'xtable', 'Hmisc', 'mixtools',
+                   'gridExtra', 'sparseLDA', 'e1071', 'klaR', 'kernlab',
+                   'devtools')
+install.packages(cran_packages, dep=TRUE)
+
+github_packages <- c('ramhiser/datamicroarray', 'ramhiser/sparsediscrim')
+library(devtools)
+devtools::install_github(github_packages)
