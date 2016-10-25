737b3cb5bccf59c2372f6f20752788e316e66449
John Ramey <johnramey@gmail.com>
2016-10-25 16:12:06 -0500

    Updated timing comparisons. #26
    Timing comparison across several values of `p`.

--- timing-comparison.r
+++ timing-comparison.r
@@ -5,8 +5,7 @@ library(microbenchmark)
 load.project()
 
 # Number of timing-comparison replications to perform
-# num_reps <- 50
-num_reps <- 5
+num_reps <- 100
 
 # Grid Size (m x m)
 grid_size <- 5
@@ -14,39 +13,31 @@ grid_size <- 5
 # Simulation Parameters
 K <- 4
 n <- rep.int(x=25, times=K)
-# p <- c(10, 100, 1000, 10000)
-p <- c(10, 20)
-
-# TODO: Update means
-means <- lapply(c(-3, -1, 1, 3), rep.int, times=p)
-cov_mat <- diag(p)
-
-# Generates multivariate normal data
-set.seed(42)
-data <- simdata_normal(n=n, mean=means, cov=cov_mat)
+feature_dims <- c(
+  25, 50, 75,
+  seq(100, 1000, by=100),
+  seq(1500, 10000, by=500)
+)
 
 # klaR's Implementation of Friedman (1989)
 rda_model_selection <- function(x, y, num_tuning=5) {
   tuning_grid <- expand.grid(lambda=seq(0, 1, length=num_tuning),
                              gamma=seq(0, 1, length=num_tuning))
+   rda_errors <- vector(mode="list", length=nrow(tuning_grid))
+   for (i in seq_len(nrow(tuning_grid))) {
+     rda_errors[[i]] <- try({
+       rda_out <- rda(x=x,
+                      grouping=y,
+                      lambda=tuning_grid$lambda[i],
+                      gamma=tuning_grid$gamma[i],
+                      estimate.error=FALSE)
+       rda_out$error.rate['crossval']
+     })
+   }
 
-  rda_errors <- vector(mode="list", length=nrow(tuning_grid))
-
-  for (i in seq_len(nrow(tuning_grid))) {
-    rda_errors[[i]] <- try({
-      rda_out <- rda(x=x,
-                     grouping=y,
-                     lambda=tuning_grid$lambda[i],
-                     gamma=tuning_grid$gamma[i],
-                     estimate.error=FALSE)
-      rda_out$error.rate['crossval']
-    })
-  }
-
-  rda_errors[!sapply(rda_errors, is.numeric)] <- NA
-
-  tuning_grid[which.min(unlist(rda_errors)), ]
-}  
+   rda_errors[!sapply(rda_errors, is.numeric)] <- NA
+   tuning_grid[which.min(unlist(rda_errors)), ]
+}
 
 # HDRDA
 hdrda_model_selection <- function(x, y, num_tuning=5) {
@@ -57,10 +48,25 @@ hdrda_model_selection <- function(x, y, num_tuning=5) {
            shrinkage_type="convex")
 }
 
-timing_results <- microbenchmark(
-  rda_model_selection(x=data$x, y=data$y, num_tuning=grid_size),
-  hdrda_model_selection(x=data$x, y=data$y, num_tuning=grid_size),
-  times=num_reps)
+# Timing comparison for various values of p
+timing_results <- lapply(feature_dims, function(p) {
+  # Creates a list of 4 mean vectors. Each vector has a length of `p`.
+  means <- lapply(c(-3, -1, 1, 3), rep.int, times=p)
+  cov_mat <- diag(p)
 
-save(timing_results, file="data/timing-results.RData") 
+  # Generates multivariate normal data
+  set.seed(42)
+  data <- simdata_normal(n=n, mean=means, cov=cov_mat)
 
+  mb_results <- microbenchmark(
+    rda_model_selection(x=data$x, y=data$y, num_tuning=grid_size),
+    hdrda_model_selection(x=data$x, y=data$y, num_tuning=grid_size),
+    times=num_reps)
+  cat("Timing results for p =", p, "\n")
+  print(mb_results)
+
+  mb_results
+})
+names(timing_results) <- feature_dims
+
+save(timing_results, file="data/timing-results.RData")
