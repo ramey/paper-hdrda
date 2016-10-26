46df7096ab193589cf69e97de3dd355d3b4a944c
John Ramey <johnramey@gmail.com>
2016-10-26 20:11:48 +0000

    Added microbenchmark package to requirements

--- requirements.r
+++ requirements.r
@@ -4,7 +4,7 @@ biocLite("multtest", ask=FALSE)
 cran_packages <- c('futile.logger', 'reshape2', 'plyr', 'ggplot2', 'stringr',
                    'rda', 'penalizedLDA', 'xtable', 'Hmisc', 'mixtools',
                    'gridExtra', 'sparseLDA', 'e1071', 'klaR', 'kernlab',
-                   'devtools', 'randomForest', 'ProjectTemplate', 'sortinghat')
+                   'devtools', 'randomForest', 'ProjectTemplate', 'sortinghat', 'microbenchmark')
 install.packages(cran_packages, dep=TRUE)
 
 github_packages <- c('ramhiser/datamicroarray', 'ramhiser/sparsediscrim',
